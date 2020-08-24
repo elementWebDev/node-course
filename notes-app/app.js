7b62fb4475898f4163733ae3eee707d69f61ea9a
elementwebdev <ivinci.design@gmail.com>
2020-08-24 10:06:11 -0700

    Revert "Add lesson 9"
    This reverts commit c8afbc75dc390a4329b246a65782678b339e95b1.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,18 @@
-// local files must have relative links
-require('./utils.js')
+const getNotes = require('./notes.js')
 
-const name = 'Ivan'
+const msg = getNotes()
 
-console.log(name)
+console.log(msg)
+
+//
+// Challenge: Define and use a function in a new file
+//
+// 1. Create a new file called notes.js
+// 2. Create getNotes function that returns "Your notes..."
+// 3. Export getNotes function
+// 4. From app.js, load in and call the function printing message to console
+
+// const add = require('./utils.js')
+// const sum = add(4, -2)
+// console.log(sum)
+
