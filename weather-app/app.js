180f0e381f09998a8c64355157d4344b47b1392d
elementwebdev <ivinci.design@gmail.com>
2020-09-18 12:38:01 -0700

    --amend

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,10 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=e2e260996b1cb4ae50acbcf5faca050b&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=e2e260996b1cb4ae50acbcf5faca050b&query=37.8267,-122.4233&units=f'
 
 request({ url: url, json: true }, (error, response) => {
     // console.log(response.body.current);
-    console.log('It is currently ' + response.body.current.temperature + ' degrees. It feels like ' + response.body.current.feelslike + ' degrees.');
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees. It feels like ' + response.body.current.feelslike + ' degrees.');
 })
 
 //
