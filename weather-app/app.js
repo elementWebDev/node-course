175af414a8084203e981eb68588ff168069aac01
elementwebdev <ivinci.design@gmail.com>
2020-09-18 12:10:29 -0700

    32. Customizing HTTP requests challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,13 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=e2e260996b1cb4ae50acbcf5faca050b&query=37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.current);
+    console.log('It is currently ' + response.body.current.temperature + ' degrees. It feels like ' + response.body.current.feelslike + ' degrees.');
 })
+
+//
+// Goal: Print a small forecast to the user
+//
+// 1. Print: "It is currently 9 degrees out. It feels like 5 degrees out."
+// 2. Test your work!
