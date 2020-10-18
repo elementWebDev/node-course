4386a6f298d5da36328465182dfa9222d254f99f
elementwebdev <ivinci.design@gmail.com>
2020-10-18 15:25:49 -0700

    38. callback chaining - pre challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,33 @@
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const geocode = require("./utils/geocode")
+const forecast = require("./utils/forecast")
 
-geocode('Eugene, Oregon', (error, data) => {
-  console.log('Error: ', error)
-  console.log('Data: ', data)
+//
+// Goal: Accept location via command line argument
+//
+// 1. Access the command line argument without using yargs
+//  /*	 console.log(process.argv[2])	*/
+
+// 2. Use the string value as the input for geocode
+// 3. Only geocode if a location was provided
+// 4. Test your work with a couple locations
+
+//	if (process.argv[2]) {
+// 		... geocode()
+//	} else {
+// 		console.log('Please provide a location')
+//	}
+
+geocode("La Pine, Oregon", (error, data) => {
+	if (error) {
+		return console.log(error)
+	}
+
+	forecast(data.latitude, data.longitude, (error, forecastData) => {
+		if (error) {
+			return console.log(error)
+		}
+
+		console.log(data.location)
+		console.log(forecastData)
+	})
 })
-
-forecast(44.1545, -75.7088,  (error, data) => {
-    console.log('Error:', error)
-    console.log('Data:', data)
-  })
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,22 +1,30 @@
-const request = require('request')
+const request = require("request")
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=e2e260996b1cb4ae50acbcf5faca050b&query=' + latitude + ',' + longitude + '&units=f'
+	const url =
+		"http://api.weatherstack.com/current?access_key=e2e260996b1cb4ae50acbcf5faca050b&query=" +
+		latitude +
+		"," +
+		longitude +
+		"&units=f"
 
-    request({ url: url, json: true }, (error, response) => {
-
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
-            callback('Unable to find location', undefined)
-
-        } else {
-            callback(undefined, response.body.current.weather_descriptions[0] +
-                '. It is currently ' + response.body.current.temperature +
-                ' degrees. It feels like ' + response.body.current.feelslike +
-                ' degrees')
-        }
-    })
+	request({ url: url, json: true }, (error, response) => {
+		if (error) {
+			callback("Unable to connect to weather service!", undefined)
+		} else if (response.body.error) {
+			callback("Unable to find location", undefined)
+		} else {
+			callback(
+				undefined,
+				response.body.current.weather_descriptions[0] +
+					". It is currently " +
+					response.body.current.temperature +
+					" degrees. It feels like " +
+					response.body.current.feelslike +
+					" degrees"
+			)
+		}
+	})
 }
 
 module.exports = forecast
