d461495a572fcdc0edb6babbeaf7dea494bb18b8
elementwebdev <ivinci.design@gmail.com>
2020-10-21 16:17:27 -0700

    44. Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,17 +2,35 @@ const express = require('express')
 
 const app = express()
 
-// What to do once getting to a specified route
+// Send HTML
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
+// Send JSON data
+// Can also send array of objects
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send({
+        name: 'Ivan',
+        age: 40
+    })
 })
+
+//
+// Goal: Update routes
+//
+// 1. Setup about route to render a title with HTML
+// 2. Setup a weather route to send back JSON
+//  - Object with forecast and location strings
+// 3. Test work by visiting both in the browser
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send([{
+        name: 'Sara'
+    },
+    {
+        name: 'Ivan'
+    }])
 })
 
 app.get('/weather', (req, res) => {
