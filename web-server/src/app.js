9a661aae47f2b5fdac518ce300447407341be3c6
elementwebdev <ivinci.design@gmail.com>
2020-10-22 09:11:33 -0700

    47. Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,9 +8,30 @@ const publicDirectoryPath = path.join(__dirname, "../public")
 // customize server
 app.use(express.static(publicDirectoryPath))
 
-// Send JSON data
-// Can also send array of objects
+// which templating engine to use
+app.set('view engine', 'hbs')
 
+// render index template
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ivan Huddleston'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Ivan Huddleston'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        msg: 'We\'re here to help!'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
