c857843606c32158242d2689b407e41e830e6df8
elementwebdev <ivinci.design@gmail.com>
2020-10-22 05:57:59 -0700

    45. Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,32 +1,16 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-// Send HTML
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+const publicDirectoryPath = path.join(__dirname, "../public")
+
+// customize server
+app.use(express.static(publicDirectoryPath))
 
 // Send JSON data
 // Can also send array of objects
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'Ivan',
-        age: 40
-    })
-})
 
-//
-// Goal: Update routes
-//
-// 1. Setup about route to render a title with HTML
-// 2. Setup a weather route to send back JSON
-//  - Object with forecast and location strings
-// 3. Test work by visiting both in the browser
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
 
 app.get('/weather', (req, res) => {
     res.send({
