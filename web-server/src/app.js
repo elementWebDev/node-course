a6ed048e710906c68ccb9220ff4b1851cd3a8ae9
elementwebdev <ivinci.design@gmail.com>
2020-10-23 11:47:36 -0700

    Update app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -51,7 +51,7 @@ app.get('/weather', (req, res) => {
         })
     } else {
 
-    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        geocode(req.query.address, (error, { latitude, longitude, location }) => {
 			if (error) {
 				return res.send({ error })
 			}
