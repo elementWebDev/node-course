aab3cd5f494ae0da67c553fdf931eb9e6cffa41b
elementwebdev <ivinci.design@gmail.com>
2020-10-26 08:50:00 -0700

    57. Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,23 @@
 console.log('Client side javascript file is loaded!')
+
+
+fetch("http://localhost:3000/weather?address=boston").then((response) => {
+    response.json().then((weather) => {
+        if (weather.error) {
+            console.log(weather.error)
+        } else {
+            console.log(weather)
+            console.log(weather.location)
+            console.log(weather.forecast)
+        }
+    })
+})
+
+// fetch api (browser api) - not accessible in back-end
+
+// fetch("http://puzzle.mead.io/puzzle").then((response) => {
+// 	response.json().then((data) => {
+// 		console.log(data)
+// 	})
+// })
+
