e3ab26949a12a0c13a244f81cd1ff6e0dce25ff2
elementwebdev <ivinci.design@gmail.com>
2020-10-26 09:17:13 -0700

    58. Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,23 +1,22 @@
 console.log('Client side javascript file is loaded!')
 
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch("http://localhost:3000/weather?address=boston").then((response) => {
-    response.json().then((weather) => {
-        if (weather.error) {
-            console.log(weather.error)
-        } else {
-            console.log(weather)
-            console.log(weather.location)
-            console.log(weather.forecast)
-        }
+weatherForm.addEventListener('submit', (e) => {
+    // prevent auto page reload
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((weather) => {
+            if (weather.error) {
+                console.log(weather.error)
+            } else {
+                console.log(weather.location)
+                console.log(weather.forecast)
+            }
+        })
     })
 })
-
-// fetch api (browser api) - not accessible in back-end
-
-// fetch("http://puzzle.mead.io/puzzle").then((response) => {
-// 	response.json().then((data) => {
-// 		console.log(data)
-// 	})
-// })
-
